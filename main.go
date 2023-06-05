2f05b3bad7f13b6defba964ef3d1e821a1742d54
asliddinberdiyevv <asliddinberdiyevv@gmail.com>
2023-06-05 21:01:51 +0500

    done

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/skip2/go-qrcode"
+)
+
+func main() {
+	if err := qrcode.WriteColorFile("https://example.org", qrcode.Medium, 256, color.Black, color.White, "qr.png"); err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Create qr code")
+	}
+
+}
